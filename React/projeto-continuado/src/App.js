202d9778417a915458b6827bd29578586eb7dd87
viniboy <viniciusalmeidafreitas@gmail.com>
2022-06-06 03:23:40 -0300

    Renderizando listas com react

---
+++ React/projeto-continuado/src/App.js
@@ -0,0 +1,54 @@
+const costumerList = [
+  {
+    id: 1,
+    name: 'Vinícius',
+    skills: ['React', 'HMTL', 'CSS', 'Typescript'],
+  },
+  {
+    id: 2,
+    name: 'Leonardo',
+    skills: ['Node', 'Jest','Typescript'],
+  },
+  {
+    id: 3,
+    name: 'Danilo',
+    skills: ['UX-UI', 'Desing', 'Photoshop',],
+  },
+  {
+    id: 4,
+    name: 'Lucas',
+    skills: ['Java'],
+  },
+];
+
+
+const renderCostumers = (costumer, index) => {
+  return (
+    <div key={`costumer-${costumer.id}`}>
+      <li>{costumer.name}</li>
+      {costumer.skills.map(renderCostumerSkills)}
+    </div>
+  )
+}
+
+const renderCostumerSkills = (skill, index) => {
+  return (
+    <div key={index} style={{paddingLeft: '30px'}}>
+      <li>{skill}</li>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      Costumer List
+
+      <ul>
+        {costumerList.map(renderCostumers)}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
